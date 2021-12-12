c9a82bc8c8f645a25c37f328a29cfae19bbff374
Mylene <pont.mylene@gmail.com>
2021-12-12 12:25:19 +0900

    all files imported, app running

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react"
+import "./style.css";
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import data from "./data"
+
 
 export default function App() {
     const cards = data.map(item => {
--- src/data.js
+++ src/data.js
@@ -1,13 +1,3 @@
-Whenever you start playing around with the code, a Note will be created. All your notes are listed below. To save a Note, click the green checkmark icon.
-Section Intro & Figma file
-4:17
-Section 2 recap
-Expand for more info
-Section Intro & Figma file
-4:17
-data.js
-12345678910111213141516171819202122232425262728
-export default [    {        id: 1,        title: "Life Lessons with Katie Zaferes",        description: "I will share with you what I call \"Positively Impactful Moments of         Disappointment.\" Throughout my career, many of my highest moments only came after setbacks         and losses. But learning from those difficult moments is what gave me the ability to rise         above them and reach my goals.",        price: 136,        coverImg: "katie-zaferes.png",        stats: {            rating: 5.0,            reviewCount: 6        },        location: "Online",        openSpots: 0,    },    {        id: 2,        title: "Learn Wedding Photography",        description: "Interested in becoming a wedding photographer? For beginner and experienced         photographers alike, join us in learning techniques required to leave the happy couple with         memories that'll last a lifetime.",        price: 125,        coverImg: "wedding-photography.png",        stats: {            rating: 5.0,            reviewCount: 30        },        location: "Online",        openSpots: 27,    },    {
 export default [
     {
         id: 1,
@@ -15,15 +5,37 @@ export default [
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
         coverImg: "katie-zaferes.png",
-
-Find
-No Results
-
-CONSOLE
-/index.html
-
-scrimba
-Learn React for free (updated)
-Build an AirBnb Experiences clone
-Section 2 recap
-
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    }
+]
